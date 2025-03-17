9fcdffb772f778bfa61ca7e961e392c0a6b551a4
Suirenka <vivian_hana@outlook.com>
2025-03-17 13:23:09 +1100

    Fix pipeline

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,6 +3,6 @@
 # documentation: https://learn.microsoft.com/en-us/azure/app-service/quickstart-python?tabs=flask%2Cmac-linux%2Cazure-portal%2Czip-deploy%2Cdeploy-instructions-azportal%2Cterminal-bash%2Cdeploy-instructions-zip-azcli
 RESOURCE_GROUP_NAME='sunshield_group'
 APP_SERVICE_NAME='sunshield'
-ZIP_PATH=’/Users/an/Documents/Monash/FIT5120/Mock\ Proj/SUNSHIELD/backend/sun-shield.zip‘
+ZIP_PATH='/Users/an/Documents/Monash/FIT5120/MockProj/SUNSHIELD/backend/sun-shield.zip'
 
 az webapp deploy --name $APP_SERVICE_NAME --resource-group $RESOURCE_GROUP_NAME --src-path $ZIP_PATH
